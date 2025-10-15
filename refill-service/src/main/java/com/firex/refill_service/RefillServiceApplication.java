4b8cd2989fc273e9b359dbe0f3fd290cabf32b43
DieDev9 <eldiegofonse@gmail.com>
2025-10-15 12:56:03 -0500

    migracion a arquitectura de microservicios

---
+++ refill-service/src/main/java/com/firex/refill_service/RefillServiceApplication.java
@@ -0,0 +1,13 @@
+package com.firex.refill_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefillServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefillServiceApplication.class, args);
+	}
+
+}
---
+++ refill-service/src/test/java/com/firex/refill_service/RefillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firex.refill_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefillServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
